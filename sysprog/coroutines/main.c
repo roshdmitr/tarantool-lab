4e6c8ebad9558ad65e148c1b42ad80b68c8c8ac1
roshdmitr <44143857+roshdmitr@users.noreply.github.com>
2018-10-16 15:14:04 +0300

    Add files via upload

--- sysprog/coroutines/main.c
+++ sysprog/coroutines/main.c
@@ -1,219 +1,208 @@
 #define _XOPEN_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <ucontext.h>
 #include <string.h>
 
-static ucontext_t context_main, context1, context2;
-static int* int_file_array1;      //we can't send to makecontext's function associated with context
-static int* int_file_array1_size; //anything but integers so we'll declare the
-static int* int_file_array2;      //arrays globally to sort them in contexts
-static int* int_file_array2_size;
+#define n 2
 
-static void quick_sort_context1(int left, int right) { //just a quick sort algorithm for context1 with switches to context2
-    int i = left, j = right;                           //sorts int_file_array1
-    swapcontext(&context1, &context2);
+static ucontext_t context_main;
+static ucontext_t contexts[n];
+static int** int_file_array;
+static int* int_file_array_size;
+
+#define context_swap(i) {                                \
+    if (((i) + 1) == n) {                                \
+        swapcontext(&contexts[(i)], &contexts[0]);       \
+    }                                                    \
+    else {                                               \
+        swapcontext(&contexts[(i)], &contexts[(i) + 1]); \
+    }                                                    \
+}
+
+static void context_function(int left, int right, int ind) { //quick sort
+    int i = left, j = right;
+    context_swap(ind);
     int tmp;
-    swapcontext(&context1, &context2);
-    int pivot = int_file_array1[(left + right) / 2];
-    swapcontext(&context1, &context2);
-    /* partition */
+    context_swap(ind);
+    int pivot = int_file_array[ind][(left + right) / 2];
+    context_swap(ind);
     while (i <= j) {
-        swapcontext(&context1, &context2);
-        while (int_file_array1[i] < pivot) {
-            swapcontext(&context1, &context2);
+        context_swap(ind);
+        while (int_file_array[ind][i] < pivot) {
+            context_swap(ind);
             i++;
-            swapcontext(&context1, &context2);
+            context_swap(ind);
         }
-        swapcontext(&context1, &context2);
-        while (int_file_array1[j] > pivot) {
-            swapcontext(&context1, &context2);
+        context_swap(ind);
+        while (int_file_array[ind][j] > pivot) {
+            context_swap(ind);
             j--;
-            swapcontext(&context1, &context2);
+            context_swap(ind);
         }
-        swapcontext(&context1, &context2);
+        context_swap(ind);
         if (i <= j) {
-            swapcontext(&context1, &context2);
-            tmp = int_file_array1[i];
-            swapcontext(&context1, &context2);
-            int_file_array1[i] = int_file_array1[j];
-            swapcontext(&context1, &context2);
-            int_file_array1[j] = tmp;
-            swapcontext(&context1, &context2);
+            context_swap(ind);
+            tmp = int_file_array[ind][i];
+            context_swap(ind);
+            int_file_array[ind][i] = int_file_array[ind][j];
+            context_swap(ind);
+            int_file_array[ind][j] = tmp;
+            context_swap(ind);
             i++;
-            swapcontext(&context1, &context2);
+            context_swap(ind);
             j--;
-            swapcontext(&context1, &context2);
+            context_swap(ind);
         }
-        swapcontext(&context1, &context2);
+        context_swap(ind);
     };
-    swapcontext(&context1, &context2);
+    context_swap(ind);
 
     if (left < j) {
-        swapcontext(&context1, &context2);
-        quick_sort_context1(left, j);
-        swapcontext(&context1, &context2);
+        context_swap(ind);
+        context_function(left, j, ind);
+        context_swap(ind);
     }
-    swapcontext(&context1, &context2);
     if (i < right) {
-        swapcontext(&context1, &context2);
-        quick_sort_context1(i, right);
-        swapcontext(&context1, &context2);
+        context_swap(ind);
+        context_function(i, right, ind);
+        context_swap(ind);
     }
-    swapcontext(&context1, &context2);
+    context_swap(ind);
 }
 
-static void quick_sort_context2(int left, int right) { //just a quick sort algorithm for context2 with switches to context1
-    int i = left, j = right;                           //sorts int_file_array2
-    swapcontext(&context2, &context1);
-    int tmp;
-    swapcontext(&context2, &context1);
-    int pivot = int_file_array2[(left + right) / 2];
-    swapcontext(&context2, &context1);
-
-    /* partition */
-    while (i <= j) {
-        swapcontext(&context2, &context1);
-        while (int_file_array2[i] < pivot) {
-            swapcontext(&context2, &context1);
-            i++;
-            swapcontext(&context2, &context1);
-        }
-        swapcontext(&context2, &context1);
-        while (int_file_array2[j] > pivot) {
-            swapcontext(&context2, &context1);
-            j--;
-            swapcontext(&context2, &context1);
-        }
-        swapcontext(&context2, &context1);
-        if (i <= j) {
-            swapcontext(&context2, &context1);
-            tmp = int_file_array2[i];
-            swapcontext(&context2, &context1);
-            int_file_array2[i] = int_file_array2[j];
-            swapcontext(&context2, &context1);
-            int_file_array2[j] = tmp;
-            swapcontext(&context2, &context1);
-            i++;
-            swapcontext(&context2, &context1);
-            j--;
-            swapcontext(&context2, &context1);
-        }
-        swapcontext(&context2, &context1);
-    };
-    swapcontext(&context2, &context1);
-
-    if (left < j) {
-        swapcontext(&context2, &context1);
-        quick_sort_context2(left, j);
-        swapcontext(&context2, &context1);
-    }
-    swapcontext(&context2, &context1);
-    if (i < right) {
-        swapcontext(&context2, &context1);
-        quick_sort_context2(i, right);
-        swapcontext(&context2, &context1);
-    }
-    swapcontext(&context2, &context1);
-}
-
-char* read_file(FILE* f) //reading one file to memory
+void merge_arrays(int* arr1, int* arr2, int n1, int n2, int* arr3)
 {
-    fseek(f, 0, SEEK_END);
-    long size = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    char *s = (char*) malloc((size + 1) * sizeof(char));
-    fread(s, (size_t)size, 1, f);
-    s[size] = '\0';
-    printf("%s \n", s);
-    return s;
-}
+    int i = 0, j = 0, k = 0;
 
-int* parse_file(char* s, int *n) //making an array of integers from the read file
-{
-    int size = 1000;
-    int* arr = (int*) malloc(size * sizeof(int));
-    int iter = 0;
-    int bytes_read, c, length = 0;
-    while(sscanf(s, "%d%n", &c, &bytes_read) > 0)
+    while (i < n1 && j < n2)
     {
-        if (length + 1 >= size) {
-            arr = realloc(arr, size * 2 * sizeof(int));
-            size *= 2;
-        }
-        arr[length++] = c;
-        s += bytes_read;
-        iter++;
+        if (arr1[i] < arr2[j])
+            arr3[k++] = arr1[i++];
+        else
+            arr3[k++] = arr2[j++];
     }
-    arr = realloc(arr, iter * sizeof(int));
-    *n = iter;
-    for (int i = 0; i < iter; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-    return arr;
+    while (i < n1)
+        arr3[k++] = arr1[i++];
+    while (j < n2)
+        arr3[k++] = arr2[j++];
+}
+
+void parse_file(FILE* f, int i) {
+    int t;
+    int* tmp;
+    int j = 0, size = 1000;
+    fscanf(f, "%d", &t);
+    int_file_array[i] = (int*) malloc(1000 * sizeof(int));
+    int_file_array[i][j] = t;
+    while (!feof(f)) {
+        fscanf(f, "%d", &t);
+        j++;
+        if (j > size) {
+            tmp = realloc(int_file_array[i], size * 2 * sizeof(int));
+            size *= 2;
+            int_file_array[i] = tmp;
+            tmp = NULL;
+        }
+        int_file_array[i][j] = t;
+    }
+    tmp = realloc(int_file_array[i], j * sizeof(int));
+    int_file_array[i] = tmp;
+    tmp = NULL;
+    int_file_array_size[i] = j;
 }
 
 int main() {
-    printf("Enter the number of files you want to read:\n");
-    int n;
-    scanf("%d", &n);
-    printf("Enter file names:\n");
+    printf("Enter %d file names:\n", n);
     FILE** fin = (FILE**) malloc(n * sizeof(FILE*)); //array of all input files
     FILE** fout = (FILE**) malloc(n * sizeof(FILE*)); //array for output files (sorted)
     char s[100];
     for (int i = 0; i < n; i++) {
         scanf("%s", s);
         fin[i] = fopen(s, "r");
-        fout[i] = fopen(strcat(s, "_out"), "w+t");
+        fout[i] = fopen(strcat(s, "_out"), "w+");
         if (fin[i] == NULL || fout[i] == NULL) {
             printf("error opening file");
         }
     }
 
-    static char context1_stack[63484];
-    static char context2_stack[63484];
-
-    getcontext(&context1);
-    context1.uc_stack.ss_sp = context1_stack;
-    context1.uc_stack.ss_size = sizeof(context1_stack);
-    context1.uc_link = &context_main;
-
-    getcontext(&context2);
-    context2.uc_stack.ss_sp = context2_stack;
-    context2.uc_stack.ss_size = sizeof(context2_stack);
-    context2.uc_link = &context1;
-
-    char* string_file_array1;
-    int_file_array1_size = (int*) malloc(sizeof(int)); //it was declared globally, so let's allocate some memory for it
-    char* string_file_array2;
-    int_file_array2_size = (int*) malloc(sizeof(int)); //same
-    for (int i = 0; i < n / 2; i++) {
-        string_file_array1 = read_file(fin[i]); //read the first file
-        int_file_array1 = parse_file(string_file_array1, int_file_array1_size); //make an array from the first file
-        string_file_array2 = read_file(fin[i + 1]); //read the second file
-        int_file_array2 = parse_file(string_file_array2, int_file_array2_size); //make an array from the second file
-
-        makecontext(&context1, quick_sort_context1, 2, 0, *int_file_array1_size - 1); //associate context1 with quick_sort1
-        makecontext(&context2, quick_sort_context2, 2, 0 , *int_file_array2_size - 1); //associate context2 with quick_sort2
-
-        swapcontext(&context_main, &context2);
-        for (int j = 0; j < *int_file_array1_size; j++) //print first sorted array
-            fprintf(fout[i], "%d ", int_file_array1[j]);
-        for (int j = 0; j < *int_file_array2_size; j++) //print second sorted array
-            fprintf(fout[i + 1], "%d ", int_file_array2[j]);
-        free(string_file_array1); //some cleanup because files don't fit into memory all together
-        free(int_file_array1);
-        free(string_file_array2);
-        free(int_file_array2);
+    char stacks[n][1048576];
+    for (int i = 0; i < n; i++) {
+        getcontext(&contexts[i]);
+        contexts[i].uc_stack.ss_sp = stacks[i];
+        contexts[i].uc_stack.ss_size = sizeof(stacks[i]);
+        if (i == 0)
+            contexts[i].uc_link = &context_main;
+        else
+            contexts[i].uc_link = &contexts[i - 1];
     }
-    free(int_file_array1_size);
-    free(int_file_array2_size);
+
+    int_file_array = (int**) malloc(n * sizeof(int*));        //arrays of all numbers from all files
+    int_file_array_size = (int*) malloc(n * sizeof(int));     //all sizes of arrays
 
     for (int i = 0; i < n; i++) {
+        parse_file(fin[i], i);                                //making arrays
+
+        for (int j = 0; j < int_file_array_size[i]; j++) {    //checking
+            printf("%d ", int_file_array[i][j]);
+        }
+        printf("\n");
+
+        makecontext(&contexts[i], context_function, 3, 0, int_file_array_size[i] - 1, i); //associate co-routines with sorting
+    }
+
+    swapcontext(&context_main, &contexts[n - 1]);                                         //swapping to context
+
+    for (int i = 0; i < n; i++) {                                                         //checking the sort
+        for (int j = 0; j < int_file_array_size[i]; j++) {
+            fprintf(fout[i], "%d ", int_file_array[i][j]);
+            printf("%d ", int_file_array[i][j]);
+    }
+    printf("\n");
+    }
+
+    FILE* final_file = fopen("final", "w+");
+
+    int** merged_arrays;
+    if (n > 2) {
+        merged_arrays = (int**) malloc((n - 1) * sizeof(int *));
+        int size = int_file_array_size[0] + int_file_array_size[1];
+        merged_arrays[0] = (int*) malloc(size * sizeof(int));
+        merge_arrays(int_file_array[0], int_file_array[1], int_file_array_size[0], int_file_array_size[1], merged_arrays[0]);
+        for (int i = 2; i < n; i++) {
+            size += int_file_array_size[i];
+            merged_arrays[i - 1] = (int*) malloc(size * sizeof(int));
+            merge_arrays(merged_arrays[i - 2], int_file_array[i], int_file_array_size[i - 2], int_file_array_size[i], merged_arrays[i - 1]);
+        }
+        for (int i = 0; i < size; i++)
+            fprintf(final_file, "%d ", merged_arrays[n - 2][i]);
+    }
+    else {
+        merged_arrays = (int**) malloc(sizeof(int *));
+        int size = int_file_array_size[0] + int_file_array_size[1];
+        merged_arrays[0] = (int*) malloc(size * sizeof(int));
+        merge_arrays(int_file_array[0], int_file_array[1], int_file_array_size[0], int_file_array_size[1], merged_arrays[0]);
+        for (int i = 0; i < size; i++) {
+            fprintf(final_file, "%d ", merged_arrays[0][i]);
+        }
+    }
+
+    free(int_file_array_size);       //cleanup
+    for (int i = 0; i < n; i++) {
+        free(int_file_array[i]);
         fclose(fin[i]);
         fclose(fout[i]);
     }
+    fclose(final_file);
+    if (n > 2) {
+        for (int i = 0; i < n - 1; i++) {
+            free(merged_arrays[i]);
+        }
+    }
+    else
+        free (merged_arrays[0]);
+    free(merged_arrays);
+    free(int_file_array);
     free(fin);
     free(fout);
     return 0;
